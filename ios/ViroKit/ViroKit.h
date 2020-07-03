32914cdf83c740e2072fd9a80b4631af13ab747b
Zheyuan Zhang <z464zhan@uwaterloo.ca>
2020-07-03 13:15:53 -0500

    Create new framework Virokit

---
+++ ios/ViroKit/ViroKit.h
@@ -0,0 +1,19 @@
+//
+//  ViroKit.h
+//  ViroKit
+//
+//  Created by user923284 on 7/3/20.
+//  Copyright © 2020 Viro Media. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViroKit.
+FOUNDATION_EXPORT double ViroKitVersionNumber;
+
+//! Project version string for ViroKit.
+FOUNDATION_EXPORT const unsigned char ViroKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViroKit/PublicHeader.h>
+
+
