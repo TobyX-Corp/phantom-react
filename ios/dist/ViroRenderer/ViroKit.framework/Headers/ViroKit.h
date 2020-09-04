8010e2c199f577fb4ab620893cc2f77731c9ec16
ZIYI HUA <zyhua@uwaterloo.ca>
2020-09-04 06:10:58 -0400

    Remove gvr(sounds)

--- ios/dist/ViroRenderer/ViroKit.framework/Headers/ViroKit.h
+++ ios/dist/ViroRenderer/ViroKit.framework/Headers/ViroKit.h
@@ -135,9 +135,9 @@ FOUNDATION_EXPORT const unsigned char ViroKitVersionString[];
 #import <ViroKit/VROAudioPlayer.h>
 #import <ViroKit/VROAudioPlayeriOS.h>
 #import <ViroKit/VROSound.h>
-#import <ViroKit/VROSoundGVR.h>
+//#import <ViroKit/VROSoundGVR.h>
 #import <ViroKit/VROSoundData.h>
-#import <ViroKit/VROSoundDataGVR.h>
+//#import <ViroKit/VROSoundDataGVR.h>
 #import <ViroKit/VROSoundDataDelegate.h>
 #import <ViroKit/VROSoundDelegate.h>
 #import <ViroKit/VROSoundDelegateiOS.h>
